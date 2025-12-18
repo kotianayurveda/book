ad5aa515697c34c32240d39b807b7eab04982a70
Sushan M <sushan.madelu@buildasign.com>
2025-12-18 18:59:41 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
